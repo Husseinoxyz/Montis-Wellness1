14b98896aa8ea481f221cb9cca499eec6aac46f3
Hussein Waliyu <albirunirbi@Husseins-MacBook-Air.local>
2026-06-22 14:14:49 +0000

    Update doctor team images

--- src/data/team.ts
+++ src/data/team.ts
@@ -2,25 +2,25 @@ export const team = [
   {
     name: 'Dr. Abdullah Asad Siddiqui',
     role: 'Emergency and Regenerative Medical Director',
-    image: '/Team/Doctor 1.png',
+    image: '/Team/Dr. Asad.PNG',
     bio: 'Provides expert care in acute and critical emergencies, trauma, and occupational health medicine.',
   },
   {
     name: 'Dr. Michelle Oh',
     role: 'Family and Holistic Medicine Physician',
-    image: '/Team/Doctor 2.png',
+    image: '/Team/Dr. Michelle.PNG',
     bio: 'Compassionate physician with over 10 years of experience in family and holistic medicine.',
   },
   {
     name: 'Dr. Mohammed A. Abdalqader',
     role: 'Associate Professor of Community Medicine (Family Health)',
-    image: '/Team/Doctor 3.png',
+    image: '/Team/Dr. Mohammed.PNG',
     bio: 'Experienced academician and public health researcher in family health with over 11 years of university teaching.',
   },
   {
     name: 'Dr. Tan Ing Shin',
     role: 'Aesthetic and Anti-Aging Physician Medical Director',
-    image: '/Team/Doctor 4.jpeg',
+    image: '/Team/Dr. Tan.PNG',
     bio: 'Letter of Credentialing and Privileging (LCP), Ministry of Health Malaysia.',
   },
 ] as const;
